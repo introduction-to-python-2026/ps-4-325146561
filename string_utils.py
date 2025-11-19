706b49965d5ecb8a6f2b014b447f197658596b13
325146561 <noasela2002@gmail.com>
2025-11-19 22:21:33 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,18 +1,24 @@
 def split_before_each_uppercases(formula):
+    if formula == "":
+        return []   # המקרה שחסר לך בטסטים
+    
     parts = []
     current = formula[0]
+
     for char in formula[1:]:
         if char.isupper():
             parts.append(current)
             current = char
         else:
             current += char
+
     parts.append(current)
     return parts
 
 
 def split_at_first_digit(formula):
     digit_location = 1
+
     for char in formula[1:]:
         if char.isdigit():
             break
@@ -20,6 +26,7 @@ def split_at_first_digit(formula):
 
     if digit_location == len(formula):
         return formula, 1
+
     prefix = formula[:digit_location]
     number = int(formula[digit_location:])
     return prefix, number
